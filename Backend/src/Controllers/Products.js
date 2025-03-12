19a7449cdb0773fe35de1616131c00ddfed6c294
VEERESH069 <116574954+VEERESH069@users.noreply.github.com>
2025-03-12 17:35:03 +0530

    Products.js

--- Backend/src/Controllers/Products.js
+++ Backend/src/Controllers/Products.js
@@ -2,13 +2,33 @@ const {Router}= require('express');
 const { productupload } = require('../../multer');
 const Productmodel = require('../Model/Productmodel');
 const productrouter=Router();
+const path = require('path');
 
 
-productrouter.get("/",(req,res)=>{      
-    res.send("Product router");
+productrouter.get("/get-router",async (req,res)=>{      
+    try{
+        const productfind = await Productmodel.find();
+        console.log(productfind);
+        const productimage = productfind.map((product)=>{{
+     return{
+            name:product.name,
+            description:product.description,
+            category:product.category,
+            tags:product.tags,
+            price:product.price,
+            stock:product.stock,
+            email:product.email,
+            images:product.images
+        }
+    }})
+    res.status(200).json({products:productimage});
+    }
+    catch(error){
+        console.error(error);
+    }
 })
 
-productrouter.post("/",productupload.array('files'),async (req,res)=>{
+productrouter.post("/post-product",productupload.array('files'),async (req,res)=>{
     const {name, description, category,tags,price,stock,email}=req.body;
      const images = req.files.map(file => file.path);
      try{
@@ -39,6 +59,38 @@ productrouter.post("/",productupload.array('files'),async (req,res)=>{
          console.error(error);
      }  
 })
+
+productrouter.put(".edit-product/:id",productupload.array("images",10), async (req,res) => {
+    try{
+        const {name, description, category,tags,price,stock,email}=req.body;
+        const id = req.params;
+        const isexist = await product.findOne({_id:id});
+        if(!isexist){
+            return res.status(400).json({Message:"Could not find the product"});
+        }
+        const updateimage = isexist.files;
+        if(req.files && req.files.length > 0){
+            updateimage.req.files.map((img)=>{
+                return `/product/${path.basename($img)}`
+            })
+        }
+        existproduct.name = name,
+        existproduct.description = description
+        existproduct.category = category
+        existproduct.tags = tags
+        existproduct.price = price
+        existproduct.stock = stock
+        existproduct.email = email
+        existproduct.images = updateimage
+
+        await existproduct.save();
+        return res.status(200).json({Product: existproduct})
+    }
+    catch(err){
+        return res.status(500).json({Message:"bad request putta"})
+    }
+})
+
 
 
 module.exports=productrouter;
